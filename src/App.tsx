1f340bdb0f30c783ebdcae082a964bdfbe644f82
ViktorKW <viktorkw1201@gmail.com>
2023-11-02 22:14:37 +0200

    basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import NotFoundPage from "./pages/NotFoundPage"
+import NewsPage from "./pages/NewsPage"
+import ShowsPage from "./pages/ShowsPage"
+import JobsPage from "./pages/JobsPage"
+import NavBar from "./components/NavBar"
 
 export default function App() {
+	const empty_space = <canvas className='empty-space' height={"20px"}></canvas>
+  
 	return (
-		<></>
+		<div className='app'>
+			<NavBar />
+			{empty_space}
+			<Routes>
+				<Route index element={<HomePage />} />
+				<Route path='news' element={<NewsPage />}></Route>
+				<Route path='shows' element={<ShowsPage />} />
+				<Route path='jobs' element={<JobsPage />} />
+				<Route path='*' element={<NotFoundPage />} />
+			</Routes>
+		</div>
 	)
 }
-
