d420a0caf3daa2df83ecffa75cabaa25abc154e0
ViktorKW <viktorkw1201@gmail.com>
2023-11-07 13:43:05 +0200

    added loader to ResourcePage

--- src/pages/ResourcePage.tsx
+++ src/pages/ResourcePage.tsx
@@ -1,3 +1,4 @@
+import "./ResourcePage.scss"
 import { useEffect, useState } from "react"
 import { TResource } from "../types"
 import ResourceTable from "../components/ResourceTable/ResourceTable"
@@ -10,20 +11,49 @@ interface IResourcePageProps{
 export default function ResourcePage(props:IResourcePageProps){
 	const [resources, setResources] = useState<TResource[]>([])
 	const { getResources, resourceName } = props
+	const [isLoaded, setIsLoaded] = useState<boolean>(false)
+	const [error, setError] = useState<string|null>(null)
+
+	function delay(ms:number) {
+		return new Promise((resolve) => {
+			setTimeout(resolve, ms)
+		})
+	}
 
 	useEffect(()=>{
 		async function initJobs(){
+			setIsLoaded(false)
+			setError(null)
+
 			const newResources:TResource[] = await getResources()
+			if(newResources.length === 0){
+				setError("No items has been retrieved")
+			}
+			
+			await delay(200)
 			setResources(newResources)
+			setIsLoaded(true)
 		}
 
 		initJobs()
 	}, [getResources, resourceName])
 
+	const renderResourceTable = ():JSX.Element=>{
+		if(isLoaded === false){
+			return <p className="pad-10 text-medium support-color">Loading...</p>
+		} else if(isLoaded === true && error === null){
+			return <ResourceTable resourcesArray={ resources }/>
+		} else if(isLoaded === true && error !== null){
+			return <p className="pad-10 text-large warning-color">{ error }</p>
+		} else {
+			return <p className="pad-10 text-large warning-color">Unknown Error</p>
+		}
+	}
+
 	return (
-		<>
-			<div className="text-large" style={{padding:"10px"}}>{ resourceName }</div>
-			<ResourceTable resourcesArray={ resources }/>
-		</>
+		<div className="resource-page">
+			<div className="pad-10 text-large">{ resourceName }</div>
+			{renderResourceTable()}
+		</div>
 	)
 }
