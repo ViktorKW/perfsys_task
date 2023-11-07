39fa37e4608836a55aa2d226bfe4d170569eafb5
ViktorKW <viktorkw1201@gmail.com>
2023-11-07 19:59:45 +0200

    change BrowserRouter for HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
-import { BrowserRouter } from "react-router-dom"
+import { RouterProvider, createHashRouter } from "react-router-dom"
+
+const router = createHashRouter([
+	{
+		path: "/*",
+		element: <App />,
+	}
+])
+  
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-	<BrowserRouter basename="/perfsys_task">
-		<App />
-	</BrowserRouter>,
+	<RouterProvider router={router} />
 )
